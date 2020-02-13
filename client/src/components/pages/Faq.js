f6201aa387080c8966c46a711468faa8ae58527a
NH Webmaster <nh-webmaster@mit.edu>
2020-02-13 00:28:09 -0500

    trying again

---
+++ client/src/components/pages/Faq.js
@@ -0,0 +1,243 @@
+import React, { Component } from "react";
+import './Faq.css';
+
+class Faq extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className='faq-container'>
+        <div className='about-title'>FAQ</div>
+
+        <div className='faq-item'>
+          <div className='faq-q'>
+          I just learned my room number, but where in the building am I?
+          </div>
+          <div className='faq-a'>
+          <p>Your room number in New House is in the form: 8888. The first number designates the floor you're on, then the next 3 numbers designate the room number. </p><p>If your room number is 2103, for example, this means you live on the second floor, in room 103. The doors are marked with the room numbers so you should have no problem finding your room once you're on the right floor. There are also signs outside the elevators on every floor that should help you find your room number!</p>
+
+          </div>
+        </div>
+
+        <div className='faq-item'>
+          <div className='faq-q'>
+          How do I get my room keys?
+          </div>
+          <div className='faq-a'>
+          <p>On Move-In day, just visit the New House front desk. You'll be asked to give your name and your MIT ID number (it's good to have your ID on you!). Then, you'll fill out a small form that includes mail forwarding and other contact information. And after all this, you'll get your keys. (AND WELCOME TO NEW HOUSE!!)</p>
+
+          </div>
+        </div>
+
+        <div className='faq-item'>
+          <div className='faq-q'>
+          What should I bring with me?
+          </div>
+          <div className='faq-a'>
+          <p><b>Personal:</b>
+                  <ul>
+                    <li>Usual hygenic necessities! The only things provided in the bathrooms are toilet paper and paper towels.</li>
+                    <li>Chargers for all your electronics as well as a powerstrip!</li>
+                    <li>A basic first aid kit: The house MedLinks have first aid and basic medical aid, but it's not a bad idea to bring your own! Also a fever reducer (Acetominaphin) is good to have on hand, but you can buy that once on campus as well!</li>
+                    <li> Laundry Detergent and a bag for your laundry. </li>
+                    <li>Bring your weird stuffed animals. That book you can't live without. Your keyboard and guitar. A basketball! Usually the things you don't want to show anyone are the things that will make people more interested and want to be friends with you!</li>
+                  </ul>
+                </p>
+                <p><b>For your Room:</b>
+                  <ul>
+                    <li>Your closet should have a mirror on the inside, but that said, if you want a different kind of mirror that can hang on your door or something, go for it!</li>
+                    <li>Get hangers for your clothes. The closets don't come with them, and don't have many shelves for folded clothes.</li>
+                    <li>Posters/Decorations! Carpets!</li>
+                    <li>Pillow, Sheets, and a Good blanket/comforter (it gets cold in the winter). The beds use twin extra long sheets.  Don't forget to wash them!</li>
+                    <li>If you're around in the spring or summer, you may want to bring a fan! But also know that New House is one of the few dorms that has air conditioning (And there's a temperature control for each room!).</li>
+                  </ul>
+                </p>
+                <p><b>For the Kitchen:</b>
+                  <ul>
+                    <li>Your own cookware: Some of the houses at new house have communal cookware so check with your house if you need something, but it also never hurts to have your own stuff! Especially if you have dietary restrictions, having your own cooking/dining set can be really great.</li>
+                    <li> Specifically: 1 Utensils set, 1 plate, 1 bowl, 1 microwavable cup, 1 sauce/frying pan, 1 pot for boiling things. </li>
+                    <li> Get Tupperware and ziploc bags. IF there's leftover free food and you want it, this is essential. Also, you can pack lunch and refrigerate leftovers!</li>
+                  </ul>
+                </p>
+          </div>
+        </div>
+
+        <div className='faq-item'>
+          <div className='faq-q'>
+          How many singles and doubles are available in New House to the incoming freshmen?          </div>
+          <div className='faq-a'>
+          <p>It changes every year, but in all of New House, 80% of the rooms are singles. The rest are doubles. Also highly recommend not living in a single freshman year! It's a great way to meet new people. If you have any further questions about the availability of particular kinds of rooms, feel free to reach out to the houses you're interested in! </p>
+
+          </div>
+        </div>
+
+        <div className='faq-item'>
+          <div className='faq-q'>
+          What are the average sizes of the rooms?</div>
+                    <div className='faq-a'>
+                    <p>Average size of a single: 142.08 square feet <br></br> Average size of a double: 237.73 square feet. </p>
+
+          </div>
+        </div>
+
+        <div className='faq-item'>
+          <div className='faq-q'>
+          How many kitchens are there in New House?
+          </div>
+                    <div className='faq-a'>
+                    <p>Total Number of Kitchens: 14, including the Country Kitchen, which is for all of New House. The kitchens are quite large!</p>
+
+          </div>
+        </div>
+
+        <div className='faq-item'>
+          <div className='faq-q'>
+          How many people share a bathroom?          
+          </div>
+                    <div className='faq-a'>
+                    <p>On average, four people "share" one bathroom, though all residents of New House are free to use any bathroom in the building. In other words, there is no formal bathroom allocation. Also the bathrooms are for all genders.</p>
+
+          </div>
+        </div>
+
+        <div className='faq-item'>
+          <div className='faq-q'>
+          What does "W70" mean?      
+          </div>
+                    <div className='faq-a'>
+                    <p>"W70" is New House's building designator. Each building on campus has one. Check out the <a href="http://whereis.mit.edu/"> campus map online </a> for more. </p>
+
+          </div>
+        </div>
+
+        <div className='faq-item'>
+          <div className='faq-q'>
+          New House has air conditioning? When would I ever need it?
+                    </div>
+                    <div className='faq-a'>
+                    <p>Yup, we're the only undergraduate dorm with A/C. Believe it or not, it gets hot in Boston. For the first and last few weeks of the academic year (and especially if you're here over the summer), air conditioning becomes invaluable. For the rest of the time, the system is converted over to heating to keep you nice and comfortable. Each room is equipped with a thermostat, so you can adjust the temperature to your liking.</p>
+
+          </div>
+        </div>
+        <div className='faq-item'>
+          <div className='faq-q'>
+          Do you have other cool facilities?
+                    </div>
+                    <div className='faq-a'>
+                    <p>WE HAVE SO MANY COOL THINGS. Check out the <a href="/facilities"> Facilities</a> page!</p>
+
+          </div>
+        </div>
+
+        <div className='faq-item'>
+          <div className='faq-q'>
+          I wasn't able to visit you guys :( Got any pictures of the building?
+                    </div>
+                    <div className='faq-a'>
+                    <p> There are a lot of pictures on the <a href="/facilities"> Facilities</a> page! As well as a floor plan.</p>
+
+          </div>
+        </div>
+
+        <div className='faq-item'>
+          <div className='faq-q'>
+          How much does it cost to do laundry?                    </div>
+                    <div className='faq-a'>
+              Both washers and dryers are $1.25 per load.
+          </div>
+        </div>
+
+        <div className='faq-item'>
+          <div className='faq-q'>
+          How international student-friendly is New House?                   </div>
+                    <div className='faq-a'>
+                    <p>Very. All MIT dorms, including New House, are very diverse and international-student friendly. New House in particular has a formal system called the Cultural Houses that offer unique living environments in which students live with other students interested in the same culture. We have five of these houses: iHouse (International Development House), Chocolate City, French House, German House, and Spanish House. We also have four numbered houses: House 2, House 3, House 4, and Desmond (House 5), which are are not formally designated as cultural houses, but their communities are equally diverse and welcoming. We recommend checking out the House websites and taking a visit to them all if possible. :) </p>
+          </div>
+        </div>
+        <div className='faq-item'>
+          <div className='faq-q'>
+          What dining options are available?          </div>
+                    <div className='faq-a'>
+                    <p>New House residents have a variety of eating facilities from which to choose. There is an MIT dining hall - Next House Dining - immediately adjacent to New House, and it is open every night for dinner.</p>
+                  <p>Some of our student houses also offer much more affordable meal plan option which are open to both house members and other residents of New House</p>
+                  <p>MIT's student center is open every day and includes Anna's Taqueria, Alpine Bagel and Cambridge Grill, and La Verde's, a general store with a full deli. For more information, see MIT's Campus Dining.</p>
+                  <p>Many of these locations accept TechCash, the debit system attached to your MIT I.D. card. TechCash is accepted on- and off-campus, including Dominos Pizza! (And they deliver.) </p>
+                  <p>New House has extensive fully equipped kitchen facilities, with stoves, microwaves, toasters, and sinks, and residents can store their food and cooking gear in cabinets and public refrigerators. La Verde's in the student center has grocery products, and there is a large grocery store - Trader Joe's - about a 20 minute walk from New House.  </p>
+          </div>
+        </div>
+
+        <div className='faq-item'>
+          <div className='faq-q'>
+          Are there any videos of New House I can watch?</div>
+                              <div className='faq-a'>
+                              <p>Definitely. You'll find New House related videos <a href="https://mitguidetoresidences.mit.edu/new-house" target="_blank">here</a>.</p>
+          </div>
+        </div>
+
+        <div className='faq-item'>
+          <div className='faq-q'>
+          What about the walk? New House is almost the last building on dorm row?</div>
+                              <div className='faq-a'>
+                              <p>That's true, it is. That doesn't necessarily make it far, though. New House is about an 8 minute walk to the Student Center and about 15 minutes to the end of the Infinite Corridor.</p>
+                  <p>The <a href="http://web.mit.edu/facilities/transportation/shuttles/tech_shuttle.html">Tech Shuttle</a> and Campus Saferide Shuttle also stop by New House, and are good alternatives to walking.</p>
+                  <p>Also, many students at MIT in general and New House in particular own bikes. With a bike, it is easy to get from New House to anywhere on campus in less than 10 minutes and to the Student Center in about 3.</p>
+                  <p>Security and facilities, like bike racks, are excellent on campus. There are many stores which sell bikes in Cambridge, ranging from specialty stores like Cambridge Bicycle to Target. </p>
+                  <p> Also, the community here is definitely worth it ;) and you get exercise walking! Yay! </p>          </div>
+        </div>
+
+        <div className='faq-item'>
+          <div className='faq-q'>
+          What if I have special housing needs? Anything I should know about allergies or health considerations?</div>
+                              <div className='faq-a'>
+                              <p>For New House specifically, this information may be helpful to you.</p>
+                  <p><ul>
+                    <li> New House has air conditioning </li>
+                    <li> We have handicap accessible kitchens </li>
+                    <li> We have multiple handicap accessible bathrooms on every floor in every house </li>
+                    <li>No carpeting in rooms </li>
+                    <li> No smoking indoors </li>
+                    <li> No pets (except fish) </li>
+                    <li> We have elevators! </li>
+                  </ul></p>
+                  <p>The decision to give special attention in housing matters for medical, religious, or other special circumstances will be made by the Housing Office in consultation with appropriate offices. Special housing is typically granted only in exceptional cases. </p><p>Keep in mind, even if you have special circumstances, you have to enter the summer housing lottery with the rest of your class and adhere to the same deadlines. </p>
+           </div>
+        </div>
+
+        <div className='faq-item'>
+          <div className='faq-q'>
+          Do I have to clean the toilet?</div>
+                              <div className='faq-a'>
+                              <p>No, we have an awesome house management and housekeeping staff, but it's your job to keep your area clean and neat within reason. New House is your home. So, similar to how you clean up after yourself at home, try to do the same here! Your New House family will be grateful. </p>
+          </div>
+        </div>
+
+        <div className='faq-item'>
+          <div className='faq-q'>
+          Do I have to clean my room?</div>
+                              <div className='faq-a'>
+                              <p>Yes! The housekeeping staff will not take care of that for you. Keep in mind: a clean living environment facilitates productivity and good spirits, and at a sometimes stressful place like MIT, it's important to keep your surroundings clean and organized. </p>
+          </div>
+        </div>
+
+        <div className='faq-item'>
+          <div className='faq-q'>
+          My question isn't answered here :( </div>
+                              <div className='faq-a'>
+            <p>Shoot us an email! Find contact information <a href='/contact'>here</a> :)</p>
+          </div>
+        </div>
+
+
+
+
+
+
+
+      </div>
+    );
+  }
+}
+
+export default Faq;
