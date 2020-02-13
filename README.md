7e145ceabbc8c421f887bf2fca1f5375e065f084
Claire Cheng <ccheng00@mit.edu>
2020-02-13 01:01:39 -0500

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# NewHouseWebmaster
-Code behind the New House Website
-
-Website found at: https://newhousewebmaster.github.io/NewHouseWebmaster/
---
+++ precache-manifest.9a4378b926c2fec65adbfc0bef76da5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e5442f8fc2045d998ecda0c745fc6e3",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "2cb075f80a6458eefd75",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "651907a7e02234f7cfcc",
+    "url": "/react-gh-pages/static/js/2.33af256b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-gh-pages/static/js/2.33af256b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cb075f80a6458eefd75",
+    "url": "/react-gh-pages/static/js/main.e98fe0ac.chunk.js"
+  },
+  {
+    "revision": "ecfd1d276f9241e07c49",
+    "url": "/react-gh-pages/static/js/runtime-main.d24436ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
