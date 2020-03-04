bec26c88cbb9e107b3b8a6b4b429c28aef9e73c8
Claire Cheng <ccheng00@mit.edu>
2020-03-04 18:39:13 -0500

    added build directory

---
+++ build/precache-manifest.eb9177ea4c8865a758619c470bf4bc52.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7f30c7b2f3ff93c43605c57a076df4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f173db29ecee29fb5810",
+    "url": "/static/css/main.839a9d57.chunk.css"
+  },
+  {
+    "revision": "d5d50af1a92a71e838ca",
+    "url": "/static/js/2.d7d8636a.chunk.js"
+  },
+  {
+    "revision": "222b4a5f5cd655e34e2a29e8aabff23f",
+    "url": "/static/js/2.d7d8636a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f173db29ecee29fb5810",
+    "url": "/static/js/main.6529472b.chunk.js"
+  },
+  {
+    "revision": "20baadec7b860b17220b",
+    "url": "/static/js/runtime-main.1c58e4aa.js"
+  },
+  {
+    "revision": "a5f1ad503d0c898a13a915c7ea35be2b",
+    "url": "/static/media/chocolatecity.a5f1ad50.svg"
+  },
+  {
+    "revision": "c2f3489701e765ec6d4091ba01ae86e4",
+    "url": "/static/media/corridor.c2f34897.jpg"
+  },
+  {
+    "revision": "482b07befb27a905319eabc55ea3dad6",
+    "url": "/static/media/countrykitchen.482b07be.jpg"
+  },
+  {
+    "revision": "ba0534dc3aafdce6135db42a7d03cb0c",
+    "url": "/static/media/desmond.ba0534dc.svg"
+  },
+  {
+    "revision": "6e82c57e1252ec99c198b62bc27df590",
+    "url": "/static/media/floral-divider.6e82c57e.png"
+  },
+  {
+    "revision": "17e9463c224876cb107c2cdc7c6f19d2",
+    "url": "/static/media/frenchhouse.17e9463c.svg"
+  },
+  {
+    "revision": "74508cef0e9be5a6235543c746ac93a9",
+    "url": "/static/media/germanhouse.74508cef.svg"
+  },
+  {
+    "revision": "0ea559a798487a281a1a56b7cad6f47f",
+    "url": "/static/media/gym.0ea559a7.jpg"
+  },
+  {
+    "revision": "17a9b661b6b061b2823e1d4f5185253f",
+    "url": "/static/media/housefour.17a9b661.svg"
+  },
+  {
+    "revision": "b4e2f0f53bf1270bea61ea3c9eea00c5",
+    "url": "/static/media/housemaster.b4e2f0f5.jpg"
+  },
+  {
+    "revision": "04fc69378fa56a96acda8563567e6a82",
+    "url": "/static/media/housethree.04fc6937.svg"
+  },
+  {
+    "revision": "08cf10eb9379cdeec6fc0182e66b3908",
+    "url": "/static/media/housetwo.08cf10eb.svg"
+  },
+  {
+    "revision": "a27edfc782b7c4fdac045eba3797fc7f",
+    "url": "/static/media/ihouse.a27edfc7.svg"
+  },
+  {
+    "revision": "707c099da27239c2416965627b68e630",
+    "url": "/static/media/lacasa.707c099d.svg"
+  },
+  {
+    "revision": "61084697c8370d94ccbd28a74bb6be35",
+    "url": "/static/media/laundry.61084697.jpg"
+  },
+  {
+    "revision": "3b8e8250b262d56a59a47d65e47b722d",
+    "url": "/static/media/makerspace.3b8e8250.jpg"
+  },
+  {
+    "revision": "0befea983380e5350ce910ba6cee175c",
+    "url": "/static/media/minimalist-snow.0befea98.jpg"
+  },
+  {
+    "revision": "f1df15f2857b395fda38779b5ba86df2",
+    "url": "/static/media/mpr.f1df15f2.jpg"
+  },
+  {
+    "revision": "98b52222e28e4bc8b18271d7fbf6fcb2",
+    "url": "/static/media/music.98b52222.jpg"
+  },
+  {
+    "revision": "47066aa31d55f77df9d123d30c271122",
+    "url": "/static/media/newhouse-inside.47066aa3.jpg"
+  },
+  {
+    "revision": "d7a2a1fa9744b4c37636973e882be822",
+    "url": "/static/media/newhouse.d7a2a1fa.jpg"
+  },
+  {
+    "revision": "6cd901e1a2f680bb0375fe7587b300c4",
+    "url": "/static/media/study.6cd901e1.jpg"
+  }
+]);
